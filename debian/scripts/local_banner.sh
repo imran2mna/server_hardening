37a3736e535f3368776a3bb668f789133cce80b7
Imran Mohamed <imranen08@gmail.com>
2021-06-11 22:49:09 -0400

    updating RHEL 7/8 and removing unwanted

--- debian/scripts/local_banner.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/env bash
-
-grep "Banner $1" /etc/issue && grep "{{ banner_msg }}" /etc/issue
--- suse-11/scripts/aide_init_rhel.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/env bash
-
-aide -version || exit 2
-
-if [ -f /var/lib/aide/aide.db.gz ]; then
-	echo "Aide already initialized." && exit 0
-fi
-
-aide --init && mv /var/lib/aide/aide.db.new.gz /var/lib/aide/aide.db.gz && exit 0 
