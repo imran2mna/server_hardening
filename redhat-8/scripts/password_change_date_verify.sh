d8ba135c18057f20a22f79e2609b309c95a4f945
Imran Mohamed <imranen08@gmail.com>
2021-06-11 23:23:22 -0400

    modification for rhel-7/8

--- redhat-8/scripts/password_change_date_verify.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/env bash
-for usr in $(cut -d: -f1 /etc/shadow)
-do
-	[[ $(chage --list $usr | grep '^Last password change' | cut -d: -f2) > $(date) ]] && echo "$usr:$(chage --list $usr | grep '^Last password change' | cut -d: -f2)" || [ 1 -eq 1 ]
-
-done
